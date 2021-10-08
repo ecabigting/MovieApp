c465083343a910825bb80ef10b5e6cef5e9a7935
ecabigting <ericcabigting@outlook.com>
2021-10-08 18:53:38 +0400

    Finish the main hero banner - Clean up home component  - Added new useHomeFetch to hooks  - Added HomeImage style for home image styling  - Added home image index component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,43 +4,30 @@ import API from '../API'
 // load config
 import { POST_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config'
 // load components
+import HeroImage from './HeroImage'
 
-// hooks
+
+// hook
+import { useHomeFetch } from '../hooks/useHomeFetch'
 
 // load images
 import NoImage from '../images/no_image.png'
 
 const Home = () => {
-    const [state ,setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error,setError] = useState(false);
-
-    const fetchMovies = async (page,searchTerm = "") => {
-        try 
-        {
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm,page);
-
-            setState(prev => ({ // triple dots ... means spread in js
-                ...movies,
-                results:
-                    // not never mutate the state in react
-                    // if the page is greater than one append the old movies(...prev) with the new movies(...movies) 
-                    page > 1 ? [...prev.results,...movies.results] : [...movies.results]
-            }));
-        }catch(error){ setError(true); }
-
-        setLoading(false);
-    }
-
-    useEffect(()=>{
-        fetchMovies(1);
-    },[]); // the comma with [] is called dependency array for useEffect to know when to trigger
-
+    const {state,loading,error} = useHomeFetch();
     console.log(state);
-    return <div>Home</div>
+    return (
+        <>
+            {state.results[0] ? 
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                /> 
+                : null
+            }
+        </>
+    );
 };
 
 export default Home;
