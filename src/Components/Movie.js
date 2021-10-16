480bc10f2023b056a020418454161b6b454b1569
ecabigting <ericcabigting@outlook.com>
2021-10-16 15:29:28 +0400

    setup new useMovieFetch hook - update movie component

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,5 +1,24 @@
 import react from "react";
+import { useParams } from "react-router";
+// config
+import {IMAGE_BASE_URL, POSTER_SIZE } from '../config'
+// components
+import Grid from './Grid'
+import Spinner from './Spinner'
+// hook
+import useMoveFetch, { useMovieFetch } from '../hooks/useMovieFetch'
+// image
+import NoImage from '../images/no_image.png'
 
-const Movie = ({movieId}) => <div> Movie : {movieId}</div>
+const Movie = () => {
+    const { movieId } = useParams();
+    const { state:movie, loading, error } = useMovieFetch(movieId)
+    console.log(movie)
+    return (
+        <>
+            <div>Movie</div>
+        </>
+    )
+}
 
 export default Movie;
