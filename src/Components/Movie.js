91f9c7ad382a45caaa4469a7d4bf671d813b4203
ecabigting <ericcabigting@outlook.com>
2021-10-17 22:06:23 +0400

    Added breadcrumb component

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -5,20 +5,22 @@ import {IMAGE_BASE_URL, POSTER_SIZE } from '../config'
 // components
 import Grid from './Grid'
 import Spinner from './Spinner'
+import BreadCrumb from './BreadCrumb'
 // hook
-import useMoveFetch, { useMovieFetch } from '../hooks/useMovieFetch'
+import { useMovieFetch } from '../hooks/useMovieFetch'
 // image
 import NoImage from '../images/no_image.png'
 
 const Movie = () => {
     const { movieId } = useParams();
     const { state:movie, loading, error } = useMovieFetch(movieId)
-    console.log(movie)
+    if(loading) return <Spinner/>
+    if(error) return <div>Ooops.. Something went wrong!</div>
     return (
         <>
-            <div>Movie</div>
+            <BreadCrumb movieTitle={movie.original_title} />
         </>
-    )
+    );
 }
 
 export default Movie;
